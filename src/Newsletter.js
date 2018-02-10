62d277c59dff3d5a73ba778035a634ff43520fdf
Nicholas Thompson <ncthom91@gmail.com>
2018-02-10 13:07:41 -0500

    Add mailchimp integration

--- src/Newsletter.js
+++ src/Newsletter.js
@@ -11,15 +11,22 @@ class Newsletter extends Component {
           Get updates on new audio tools, audio plugins, and more.
         </SplitCell>
         <SplitCell>
-          <form className="Newsletter__Form">
+          <form className="Newsletter__Form validate" action="https://creativeintent.us16.list-manage.com/subscribe/post?u=df972a422ecdf06989756c3b0&amp;id=e2925c2ec3" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" target="_blank" novalidate>
             <input
+              required
               className="Newsletter__TextInput"
-              type="text"
-              name="email"
+              type="email"
+              name="EMAIL"
+              id="mce-EMAIL"
               placeholder="Enter your email address" />
+            {/* A bot will fill this out... a human wont. */}
+            <div style={{position: 'absolute', left: '-5000px'}} aria-hidden="true">
+              <input type="text" name="b_df972a422ecdf06989756c3b0_e2925c2ec3" tabindex="-1" value="" />
+            </div>
             <input
               className="Newsletter__Submit"
               type="submit"
+              id="mc-embedded-subscribe"
               value="Subscribe" />
           </form>
         </SplitCell>
